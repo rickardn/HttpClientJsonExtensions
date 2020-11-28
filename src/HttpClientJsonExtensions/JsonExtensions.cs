013233f6dfd89c4987de57340509bffdcfb43f5e
Rickard Nilsson <rickard.nilsson@ninetech.com>
2020-11-28 23:45:26 +0100

    Initial commit

---
+++ src/HttpClientJsonExtensions/JsonExtensions.cs
@@ -0,0 +1,85 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace HttpClientJsonExtensions
+{
+    public static class HttpClientJsonExtensions
+    {
+        /// <summary>
+        /// Send a POST request to the specified Uri as an asynchronous operation.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="client">The <see cref="HttpClient"/></param>
+        /// <param name="requestUri">The Uri the request is sent to.</param>
+        /// <param name="requestObject">The HTTP request content sent to the server.</param>
+        /// <returns>
+        /// The task object representing the asynchronous operation.
+        /// </returns>
+        public static async Task<HttpResponseMessage> PostAsync<T>(
+            this HttpClient client,
+            string requestUri,
+            T requestObject)
+        {
+            return await client.PostAsync(requestUri, new JsonContent(requestObject));
+        }
+
+        public static async Task<HttpResponseMessage> PutAsync<T>(
+            this HttpClient client,
+            string requestUri,
+            T requestObject)
+        {
+            return await client.PutAsync(requestUri, new JsonContent(requestObject));
+        }
+
+        public static async Task<HttpResponseMessage> PatchAsync<T>(
+            this HttpClient client,
+            string requestUri,
+            T requestObject)
+        {
+            return await client.PatchAsync(requestUri, new JsonContent(requestObject));
+        }
+
+        public static async Task<T> GetAsync<T>(this HttpClient client, string requestUri)
+        {
+            return (await client.GetAsync(requestUri)).Content<T>();
+        }
+
+        public static async Task<T> SendAsync<T>(this HttpClient client, HttpRequestMessage request)
+        {
+            return (await client.SendAsync(request)).Content<T>();
+        }
+    }
+
+    public static class HttpResponseMessageJsonExtensions
+    {
+        public static T Content<T>(this HttpResponseMessage response)
+        {
+            Stream stream = response.Content.ReadAsStreamAsync().Result;
+            var jsonReader = new JsonTextReader(new StreamReader(stream));
+            return new JsonSerializer().Deserialize<T>(jsonReader);
+        }
+    }
+
+    public static class HttpRequestMessageJsonExtensions
+    {
+        public static HttpRequestMessage WithContent<T>(this HttpRequestMessage request, T content)
+        {
+            request.Content = new JsonContent(content);
+            return request;
+        }
+    }
+
+    internal class JsonContent : StringContent
+    {
+        private const string JsonContentType = "application/json";
+
+        public JsonContent(object content)
+            : base(JsonConvert.SerializeObject(content))
+        {
+            Headers.ContentType = new MediaTypeHeaderValue(JsonContentType);
+        }
+    }
+}
---
+++ src/tests/HttpClientJsonExtensions.Tests/HttpClientJsonSpec.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+
+namespace HttpClientJsonExtensions.Tests
+{
+    public class HttpClientJsonScenarios
+    {
+        private const string JsonMediaType = "application/json";
+        private MessageHandlerMock _messageHandler;
+        private HttpClient _httpClient;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _messageHandler = new MessageHandlerMock();
+            _httpClient = new HttpClient(_messageHandler)
+            {
+                BaseAddress = new Uri("http://localhost")
+            };
+        }
+
+        [Test]
+        public async Task ShouldReturnDeserializedObject()
+        {
+            _messageHandler.ReturnsForAnyArgs(
+                new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(
+                        @"{ ""prop"":""value"" }",
+                        Encoding.UTF8,
+                        JsonMediaType)
+                });
+
+            var response = await _httpClient.GetAsync<Foo>("/");
+            
+            response.Prop.ShouldBe("value");
+        }
+
+        [Test]
+        public async Task ShouldReturnHttpResponseMessage()
+        {
+            _messageHandler.ReturnsForAnyArgs(new HttpResponseMessage(HttpStatusCode.OK));
+
+            var response = await _httpClient.PostAsync("/", new Foo{Prop = "value"});
+
+            _messageHandler.Request.Method.ShouldBe(HttpMethod.Post);
+            _messageHandler.Request.Content.Headers.ContentType.MediaType.ShouldBe(JsonMediaType);
+            _messageHandler.Request.Content.ReadAsStringAsync().Result.ShouldBe(@"{""Prop"":""value""}");
+        }
+    }
+
+    public class MessageHandlerMock : HttpMessageHandler
+    {
+        private HttpResponseMessage _returns;
+
+        public void ReturnsForAnyArgs(HttpResponseMessage returns)
+        {
+            _returns = returns;
+        }
+
+        public HttpRequestMessage Request { get; set; }
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            Request = request;
+            return await Task.FromResult(_returns);
+        }
+    }
+
+    public class Foo
+    {
+        public string Prop { get; set; }
+    }
+}
