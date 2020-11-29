6ac83dc667816fadff0a5c6416d7f0277e354208
Rickard Nilsson <rickard.nilsson@ninetech.com>
2020-11-29 01:33:52 +0100

    json camel case serialization

--- src/HttpClientJsonExtensions/JsonExtensions.cs
+++ src/HttpClientJsonExtensions/JsonExtensions.cs
@@ -1,22 +1,24 @@
 ﻿using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace HttpClientJsonExtensions
 {
     public static class HttpClientJsonExtensions
     {
         /// <summary>
-        /// Send a POST request to the specified Uri as an asynchronous operation.
+        ///     Send a POST request to the specified Uri as an asynchronous operation.
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="client">The <see cref="HttpClient"/></param>
+        /// <param name="client">The <see cref="HttpClient" /></param>
         /// <param name="requestUri">The Uri the request is sent to.</param>
         /// <param name="requestObject">The HTTP request content sent to the server.</param>
         /// <returns>
-        /// The task object representing the asynchronous operation.
+        ///     The task object representing the asynchronous operation.
         /// </returns>
         public static async Task<HttpResponseMessage> PostAsync<T>(
             this HttpClient client,
@@ -45,6 +47,11 @@ namespace HttpClientJsonExtensions
         public static async Task<T> GetAsync<T>(this HttpClient client, string requestUri)
         {
             return (await client.GetAsync(requestUri)).Content<T>();
+        }
+
+        public static async Task<T> DeleteAsync<T>(this HttpClient client, string requestUri)
+        {
+            return (await client.DeleteAsync(requestUri)).Content<T>();
         }
 
         public static async Task<T> SendAsync<T>(this HttpClient client, HttpRequestMessage request)
@@ -77,9 +84,21 @@ namespace HttpClientJsonExtensions
         private const string JsonContentType = "application/json";
 
         public JsonContent(object content)
-            : base(JsonConvert.SerializeObject(content))
+            : base(SerializeObject(content))
         {
             Headers.ContentType = new MediaTypeHeaderValue(JsonContentType);
+        }
+
+        private static string SerializeObject(object content)
+        {
+            var serializer = new JsonSerializer
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore
+            };
+            var stringBuilder = new StringBuilder();
+            serializer.Serialize(new JsonTextWriter(new StringWriter(stringBuilder)), content);
+            return stringBuilder.ToString();
         }
     }
 }
